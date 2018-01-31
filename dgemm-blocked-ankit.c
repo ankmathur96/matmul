007bc448246747e10e54c30ef321aca94f6b9e0a
Ankit Mathur <ankitmathur@berkeley.edu>
2018-01-31 15:27:52 -0800

    add compiling avx

--- dgemm-blocked-ankit.c
+++ dgemm-blocked-ankit.c
@@ -25,27 +25,32 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 #define min(a,b) (((a)<(b))?(a):(b))
 
 #include <immintrin.h>
+#include <stdio.h>
 
 // // assume 4x4. 
 static void do_instrinsic(double* AT, double* BT, double* CT)
 {
+  printf("Before loads into packed memory");
   __m256d b0 = _mm256_load_pd(BT);
   __m256d b1 = _mm256_load_pd(BT + 4);
   __m256d b2 = _mm256_load_pd(BT + 8);
   __m256d b3 = _mm256_load_pd(BT + 12);
+  printf("After loads into packed memory");
   // unroll this loop.
   for (int i = 0; i < 4; i++) {
     __m256d a = _mm256_load_pd(AT + i);
     __m256d c = _mm256_load_pd(CT + i);
     __m256d a_elem = _mm256_set1_pd(a[0]);
-    c = _mm_fmadd_pd(a_elem, b0, c)
-    __m256d a_elem = _mm256_set1_pd(a[1]);
-    c = _mm_fmadd_pd(a_elem, b1, c)
-    __m256d a_elem = _mm256_set1_pd(a[2]);
-    c = _mm_fmadd_pd(a_elem, b2, c)
-    __m256d a_elem = _mm256_set1_pd(a[3]);
-    c = _mm_fmadd_pd(a_elem, b3, c)
-    _mm256_store_pd(CT + i, c)
+    c = _mm256_fmadd_pd(a_elem, b0, c);
+    a_elem = _mm256_set1_pd(a[1]);
+    c = _mm256_fmadd_pd(a_elem, b1, c);
+    a_elem = _mm256_set1_pd(a[2]);
+    c = _mm256_fmadd_pd(a_elem, b2, c);
+    a_elem = _mm256_set1_pd(a[3]);
+    c = _mm256_fmadd_pd(a_elem, b3, c);
+    printf("Before store into packed memory");
+    _mm256_store_pd(CT + i, c);
+    printf("After store into packed memory");
   }
 }
 
@@ -79,9 +84,9 @@ static void do_block (int lda, int M, int N, int K, double* AT, double* BT, doub
           int N2 = min (INNER_BLOCK_SIZE, N-j);
           int K2 = min (INNER_BLOCK_SIZE, K-k);
           if (M2 != INNER_BLOCK_SIZE || N2 != INNER_BLOCK_SIZE || K2 != INNER_BLOCK_SIZE) {
-            do_block_inner(lda, M2, N2, K2, A + k + i*lda, B + j + k*lda, C + j + i*lda); 
+            do_block_inner(lda, M2, N2, K2, AT + k + i*lda, BT + j + k*lda, CT + j + i*lda); 
           } else {
-            do_instrinsic(lda, M2, N2, K2, A + i + k*lda, B + k + j*lda, C + i + j*lda)
+            do_instrinsic(AT + k + i*lda, BT + j + k*lda, CT + j + i*lda);
           }
       }
     }
@@ -93,6 +98,7 @@ static void do_block (int lda, int M, int N, int K, double* AT, double* BT, doub
  * On exit, A and B maintain their input values. */
 void square_dgemm (int lda, double* A, double* B, double* C)
 {
+  printf("Before transpose");
   double AT[lda*lda];
   for (int i = 0; i < lda; i += 1)
       for (int j = 0; j < lda; j += 1)
@@ -105,6 +111,7 @@ void square_dgemm (int lda, double* A, double* B, double* C)
   for (int i = 0; i < lda; i += 1)
       for (int j = 0; j < lda; j += 1)
           CT[i+j*lda] = C[j+i*lda];
+  printf("After transpose");
   /* For each block-row of A */
   for (int i = 0; i < lda; i += BLOCK_SIZE) {
     /* For each block-column of B */
@@ -113,13 +120,14 @@ void square_dgemm (int lda, double* A, double* B, double* C)
       /* Accumulate block dgemms into block of C */
       for (int k = 0; k < lda; k += BLOCK_SIZE)
       {
+        printf("In loop");
       	/* Correct block dimensions if block "goes off edge of" the matrix */
       	int M = min (BLOCK_SIZE, lda-i);
       	int N = min (BLOCK_SIZE, lda-j);
       	int K = min (BLOCK_SIZE, lda-k);
 
       	/* Perform individual block dgemm */
-      	do_block(lda, M, N, K, A + k + i*lda, B + j + k*lda, C + j + i*lda); 
+      	do_block(lda, M, N, K, AT + k + i*lda, BT + j + k*lda, CT + j + i*lda); 
       }
     }
   }
@@ -129,35 +137,32 @@ void square_dgemm (int lda, double* A, double* B, double* C)
           C[i+j*lda] = CT[j+i*lda];
 }
 
-
-
-
-/* This auxiliary subroutine performs a smaller dgemm operation
- *  C := C + A * B
- * where C is M-by-N, A is M-by-K, and B is K-by-N. */
-static void do_block_inner (int lda, int M, int N, int K, double* A, double* B, double* C)
-{
-  /* For each row i of A */
-  for (int i = 0; i < M; ++i)
-    /* For each column j of B */ 
-    for (int j = 0; j < N; ++j)
-    {
-      /* Compute C(i,j) */
-      double cij = C[i+j*lda];
-      int k = 0;
-      for (; k < K - 3; k += 4) {
-        int offset = k * lda + i;
-        double a = A[offset] * B[k+j*lda];
-        double b = A[offset+lda] * B[(k+1)+j*lda];
-        double c = A[offset+lda+lda] * B[(k+2)+j*lda];
-        double d = A[offset+lda+lda+lda] * B[(k+3)+j*lda];
-        cij += a + b + c + d;
-      }
-      for (; k < K; k += 1) {
-        cij += A[i+k*lda] * B[k+j*lda];
-      }
-      C[i+j*lda] = cij;
-    }
-}
+// /* This auxiliary subroutine performs a smaller dgemm operation
+//  *  C := C + A * B
+//  * where C is M-by-N, A is M-by-K, and B is K-by-N. */
+// static void do_block_inner (int lda, int M, int N, int K, double* A, double* B, double* C)
+// {
+//   /* For each row i of A */
+//   for (int i = 0; i < M; ++i)
+//     /* For each column j of B */ 
+//     for (int j = 0; j < N; ++j)
+//     {
+//       /* Compute C(i,j) */
+//       double cij = C[i+j*lda];
+//       int k = 0;
+//       for (; k < K - 3; k += 4) {
+//         int offset = k * lda + i;
+//         double a = A[offset] * B[k+j*lda];
+//         double b = A[offset+lda] * B[(k+1)+j*lda];
+//         double c = A[offset+lda+lda] * B[(k+2)+j*lda];
+//         double d = A[offset+lda+lda+lda] * B[(k+3)+j*lda];
+//         cij += a + b + c + d;
+//       }
+//       for (; k < K; k += 1) {
+//         cij += A[i+k*lda] * B[k+j*lda];
+//       }
+//       C[i+j*lda] = cij;
+//     }
+// }
 
 
